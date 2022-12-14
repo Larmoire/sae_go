d640c17d525ebbe68b657f8f8d3bc2ad8c2a0ed7
Larmoire <julienevrard148@gmail.com>
2022-12-14 15:05:09 +0100

    first commit try for particles

--- particles/new.go
+++ particles/new.go
@@ -12,12 +12,14 @@ import (
 // centre de l'écran.
 func NewSystem() System {
 	l := list.New()
-	l.PushFront(&Particle{
-		PositionX: float64(config.General.WindowSizeX) / 2,
-		PositionY: float64(config.General.WindowSizeY) / 2,
-		ScaleX:    1, ScaleY: 1,
-		ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
-		Opacity: 1,
-	})
+	for i := 0; i < (config.General.InitNumParticles); i++ {
+		l.PushFront(&Particle{
+			PositionX: float64(config.General.WindowSizeX) / 2,
+			PositionY: float64(config.General.WindowSizeY) / 2,
+			ScaleX:    1, ScaleY: 1,
+			ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
+			Opacity: 1,
+		})
+	}
 	return System{Content: l}
 }
